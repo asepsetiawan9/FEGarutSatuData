f1e2a98fd68553cf5b15c8c4a775186d4b7aa270
asepsetiawan9 <zep.setiawan09@gmail.com>
2023-04-17 14:32:50 +0700

    feat breadcrumb

--- src/pages/peta/Index.tsx
+++ src/pages/peta/Index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import dynamic from 'next/dynamic';
 import router from 'next/router';
 import React from 'react';
@@ -8,6 +7,7 @@ import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
 import kecamatanData from '../../../public/kecamatan.json';
+import BreadcrumbsWrapper from '../../components/Breadcrumbs';
 // eslint-disable-next-line import/no-named-as-default
 import Filter from './components/Filter';
 // import Tabel from './Tabel';
@@ -32,31 +32,31 @@ const Index = () => {
         />
       }
     >
-      <div className="w-full">
-        <div>bread</div>
-        <div className="flex flex-row justify-between">
-          <h1 className="pb-5 text-left">Peta Kecamatan Garut</h1>
-          <button
-            className="flex items-center space-x-2 rounded bg-blue-500 px-3 py-1 text-sm text-white hover:bg-blue-700"
-            onClick={() => router.push('/peta/pembanding')}
-          >
-            <FiColumns />
-            <span>Peta Pembanding</span>
-          </button>
-        </div>
-        <div className="py-3">
-          <Filter />
-        </div>
-        <div className="flex  w-full flex-col  justify-center">
-          <div id="map" className=" w-full">
-            <MapWithNoSSR dataKecamatan={kecamatanData} />
+      <BreadcrumbsWrapper>
+        <div className="w-full">
+          <div className="flex flex-row justify-between">
+            <h1 className="pb-5 text-left">Peta Kecamatan Garut</h1>
+            <button
+              className="flex items-center space-x-2 rounded bg-blue-500 px-3 py-1 text-sm text-white hover:bg-blue-700"
+              onClick={() => router.push('/peta/pembanding')}
+            >
+              <FiColumns />
+              <span>Peta Pembanding</span>
+            </button>
+          </div>
+          <div className="py-3">
+            <Filter />
           </div>
         </div>
-
-        <React.Suspense fallback={<p>Loading table...</p>}>
-          <Tabel data={kecamatanData} />
-        </React.Suspense>
+      </BreadcrumbsWrapper>
+      <div className="flex  w-full flex-col  justify-center">
+        <div id="map" className=" w-full">
+          <MapWithNoSSR dataKecamatan={kecamatanData} />
+        </div>
       </div>
+      <React.Suspense fallback={<p>Loading table...</p>}>
+        <Tabel data={kecamatanData} />
+      </React.Suspense>
     </Main>
   );
 };
--- src/pages/peta/Pembanding.tsx
+++ src/pages/peta/Pembanding.tsx
@@ -2,6 +2,7 @@
 import dynamic from 'next/dynamic';
 import React from 'react';
 
+import BreadcrumbsWrapper from '@/components/Breadcrumbs';
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
@@ -30,44 +31,46 @@ const Pembanding = () => {
         />
       }
     >
-      <div className="w-full">
-        <div>bread</div>
-        <div className="grid grid-cols-2 gap-3">
-          <div className="col-span-1">
-            <div className="py-3">
-              <Filter />
-            </div>
-            <div className="flex flex-col justify-center">
-              <h1 className="pb-5 text-left">Peta Kecamatan Garut</h1>
-              <div id="map" className=" w-full">
-                <MapWithNoSSR dataKecamatan={kecamatanData} />
+      <BreadcrumbsWrapper>
+        <div className="w-full">
+          <div>bread</div>
+          <div className="grid grid-cols-2 gap-3">
+            <div className="col-span-1">
+              <div className="py-3">
+                <Filter />
+              </div>
+              <div className="flex flex-col justify-center">
+                <h1 className="pb-5 text-left">Peta Kecamatan Garut</h1>
+                <div id="map" className=" w-full">
+                  <MapWithNoSSR dataKecamatan={kecamatanData} />
+                </div>
+              </div>
+              <div className="w-full">
+                <React.Suspense fallback={<p>Loading table...</p>}>
+                  <Tabel data={kecamatanData} />
+                </React.Suspense>
               </div>
             </div>
-            <div className="w-full">
-              <React.Suspense fallback={<p>Loading table...</p>}>
-                <Tabel data={kecamatanData} />
-              </React.Suspense>
-            </div>
-          </div>
-          <div className="col-span-1">
-            <div className="py-3">
-              <Filter />
-            </div>
-            <div className="flex flex-col justify-center">
-              <h1 className="pb-5 text-left">Peta Kecamatan Garut</h1>
-              <div id="map" className=" w-full">
-                <MapWithNoSSR dataKecamatan={kecamatanData} />
+            <div className="col-span-1">
+              <div className="py-3">
+                <Filter />
               </div>
-            </div>
+              <div className="flex flex-col justify-center">
+                <h1 className="pb-5 text-left">Peta Kecamatan Garut</h1>
+                <div id="map" className=" w-full">
+                  <MapWithNoSSR dataKecamatan={kecamatanData} />
+                </div>
+              </div>
 
-            <div className="w-full">
-              <React.Suspense fallback={<p>Loading table...</p>}>
-                <Tabel data={kecamatanData} />
-              </React.Suspense>
+              <div className="w-full">
+                <React.Suspense fallback={<p>Loading table...</p>}>
+                  <Tabel data={kecamatanData} />
+                </React.Suspense>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </BreadcrumbsWrapper>
     </Main>
   );
 };
