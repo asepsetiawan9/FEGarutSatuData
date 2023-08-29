abdbe4a748fb83be9e5f9d303529849104a4144d
asepsetiawan9 <zep.setiawan09@gmail.com>
2023-08-29 15:24:49 +0700

    add integrate videografis

--- src/pages/videografik/Index.tsx
+++ src/pages/videografik/Index.tsx
@@ -1,110 +1,137 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FiPackage, FiUser } from 'react-icons/fi';
 
 import BreadcrumbsWrapper from '@/components/Breadcrumbs';
+import http from '@/helpers/http';
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
 // eslint-disable-next-line import/no-named-as-default
-// import Tabel from './Tabel';
-import data from './dummy.json';
 
+type VideoDataType = {
+  desc: string;
+  url: string | undefined;
+  title: string;
+  grup: any;
+  opd: any;
+  sektoral: any;
+  id: number;
+  slug: string;
+  path: string;
+  created_at: string;
+  // desc: string;
+};
+type GrupType = string;
+type OpdType = string;
+
+type DetailDataType = {
+  url: string | undefined;
+  path: string | undefined;
+  desc: string;
+  opd: any;
+  grup: any;
+  title: string;
+};
 const Index = () => {
-  const [searchResults, setSearchResults] = useState(data);
-  const [zoomed, setZoomed] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [selectedItem, setSelectedItem] = useState(null);
+  const [data, setData] = useState<VideoDataType[]>([]);
   const [showModal, setShowModal] = useState(false);
-  const [show, setShow] = useState(8);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedOpd, setSelectedOpd] = useState('');
+  const [selectedGrup, setSelectedGrup] = useState('');
+  const [allOpd, setAllOpd] = useState<OpdType[]>([]);
+  const [allGrup, setAllGrup] = useState<GrupType[]>([]);
+  const [, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(8);
+  const [selectedItem, setSelectedItem] = useState<DetailDataType | null>(null);
 
-  const ITEMS_PER_PAGE = show;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await http().get('/videografis');
+        const resultData = response.data.videografis;
+        const resultOpd = response.data.opd;
+        const resultGrup = response.data.grup;
 
-  const handlePageChange = (page: React.SetStateAction<number>) => {
-    setCurrentPage(page);
+        setData(resultData);
+        setAllOpd(resultOpd.all_opd);
+        setAllGrup(resultGrup.all_grup);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const filteredResults = data.filter((item) => {
+    // Filter berdasarkan keyword pencarian
+    const searchMatch = item.title
+      .toLowerCase()
+      .includes(searchQuery.toLowerCase());
+
+    // Filter berdasarkan OPD yang dipilih
+    const opdMatch =
+      selectedOpd === '' || (item.opd && item.opd.name === selectedOpd);
+
+    // Filter berdasarkan Grup/Sektoral yang dipilih
+    const grupMatch =
+      selectedGrup === '' || (item.grup && item.grup.name === selectedGrup);
+
+    return searchMatch && opdMatch && grupMatch;
+  });
+
+  const handleSearchOpd = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedOpd(event.target.value);
   };
 
-  const handleSearch = (event: { target: { value: any } }) => {
-    const keyword = event.target.value;
-    let results = [];
-
-    if (keyword !== '') {
-      results = data.filter(
-        (item) =>
-          typeof item.judul === 'string' &&
-          item.judul.toLowerCase().includes(keyword.toLowerCase())
-      );
-    } else if (searchResults.length === 0 && keyword === '') {
-      results = data;
-    } else {
-      results = searchResults;
-    }
-
-    setCurrentPage(1);
-    setSearchResults(results);
+  const handleSearchGrup = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedGrup(event.target.value);
   };
 
-  const handleSearchOpd = (event: { target: { value: any } }) => {
-    const keyword = event.target.value;
-    let results = [];
-
-    if (keyword !== '') {
-      results = searchResults.filter(
-        (item) =>
-          typeof item.opd === 'string' &&
-          item.opd.toLowerCase().includes(keyword.toLowerCase())
-      );
-    } else if (searchResults.length === 0 && keyword === '') {
-      results = data;
-    } else {
-      results = searchResults;
-    }
-
-    setCurrentPage(1);
-    setSearchResults(results);
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
   };
 
-  const handleSearchSektoral = (event: { target: { value: any } }) => {
-    const keyword = event.target.value;
-    let results = [];
-
-    if (keyword !== '') {
-      results = searchResults.filter(
-        (item) =>
-          typeof item.sektoral === 'string' &&
-          item.sektoral.toLowerCase().includes(keyword.toLowerCase())
-      );
-    } else if (searchResults.length === 0 && keyword === '') {
-      results = data;
-    } else {
-      results = searchResults;
-    }
-
-    setCurrentPage(1);
-    setSearchResults(results);
-  };
-
-  const handleLihatClick = (item: React.SetStateAction<null>) => {
+  const handleLihatClick = (item: DetailDataType) => {
     setSelectedItem(item);
     setShowModal(true);
   };
 
-  const handleDownloadClick = async (item) => {
-    const response = await fetch('/dummyvideo.mp4');
-    const blob = await response.blob();
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = `${item.judul}.mp4`;
-    document.body.appendChild(a);
-    a.click();
-    a.remove();
-    window.URL.revokeObjectURL(url);
+  const handleDownloadClick = (item: {
+    url: any;
+    id?: number;
+    path: string;
+    desc?: string;
+    title: string;
+  }) => {
+    const link = document.createElement('a');
+    link.download = item.title;
+    link.href = item.url;
+    link.target = '_blank';
+    link.click();
   };
 
-  const totalPages = Math.ceil(searchResults.length / ITEMS_PER_PAGE);
-  const startItem = (currentPage - 1) * ITEMS_PER_PAGE;
-  const endItem = currentPage * ITEMS_PER_PAGE;
-  const itemsToShow = searchResults.slice(startItem, endItem);
+  const totalPageCount = Math.ceil(
+    (filteredResults?.length || 0) / itemsPerPage
+  );
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const paginatedVideografis = filteredResults?.slice(startIndex, endIndex);
+
+  const handlePageChange = (pageNumber: React.SetStateAction<number>) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handleItemsPerPageChange = (event: { target: { value: string } }) => {
+    setItemsPerPage(parseInt(event.target.value, 10));
+    setCurrentPage(1);
+  };
+  const filteredVideografis = paginatedVideografis?.filter((item) =>
+    item.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <Main
@@ -155,91 +182,87 @@ const Index = () => {
               />
             </div>
             <div className="relative col-span-2 w-full">
-              <input
-                type="text"
+              <select
                 id="search"
                 className="block w-full rounded-lg border border-[#acacac] bg-gray-50 p-1.5 pl-3 text-sm text-gray-900 focus:outline-[#fa65b1]"
-                placeholder="Pilih OPD"
+                value={selectedOpd}
                 onChange={handleSearchOpd}
-              />
+              >
+                <option value="">Pilih Opd</option>
+
+                {allOpd.map((grup, index) => (
+                  <option key={index} value={grup}>
+                    {grup}
+                  </option>
+                ))}
+              </select>
             </div>
             <div className="relative col-span-2 w-full">
-              <input
-                type="text"
+              <select
                 id="search"
                 className="block w-full rounded-lg border border-[#acacac] bg-gray-50 p-1.5 pl-3 text-sm text-gray-900 focus:outline-[#fa65b1]"
-                placeholder="Pilih Sektoral"
-                onChange={handleSearchSektoral}
-              />
-            </div>
-          </div>
-          <div className="flex flex-row justify-between">
-            <div className="text-xs font-bold">
-              {searchResults.length} Jumlah Infografik
-            </div>
+                value={selectedGrup}
+                onChange={handleSearchGrup}
+              >
+                <option value="">Pilih Sektoral</option>
 
-            <div className="flex flex-row gap-3 text-xs font-bold">
-              <span className="pt-2.5">Urutkan :</span>
-              <div>
-                <select
-                  id="underline_select"
-                  className="peer block w-full appearance-none border-0 border-b-2 border-[#acacac] bg-transparent px-3 py-2  text-gray-500 focus:border-[#acacac] focus:outline-none focus:ring-0 dark:border-gray-700 dark:text-gray-400"
-                >
-                  <option selected>Filter</option>
-                  <option value="US" selected>
-                    Terbaru
+                {allGrup.map((grup, index) => (
+                  <option key={index} value={grup}>
+                    {grup}
                   </option>
-                  <option value="US">Terpopuler</option>
-                </select>
-              </div>
+                ))}
+              </select>
             </div>
           </div>
+
           <div className="border-b-2 pt-2"></div>
           <div className="grid grid-cols-4 gap-5">
-            {itemsToShow.length === 0 ? (
+            {filteredVideografis.length === 0 ? (
               <p className="col-span-4 text-center">Data Tidak Ditemukan</p>
             ) : (
-              itemsToShow.map((item) => (
-                <div key={item.id}>
-                  <video controls className="w-full rounded-md object-cover">
-                    <source src="dummyvideo.mp4" type="video/mp4" />
-                    Sorry, your browser doesnt support embedded videos.
-                  </video>
+              (filteredVideografis.length > 0 ? filteredVideografis : data).map(
+                (item) => (
+                  <div key={item.id}>
+                    <video controls className="w-full rounded-md object-cover">
+                      <source src={item.url} type="video/mp4" />
+                      Sorry, your browser doesnt support embedded videos.
+                    </video>
 
-                  <p className="!my-1 text-center text-base font-bold">
-                    {item.judul}
-                  </p>
-                  <p className="!my-1 text-sm ">{item.description}</p>
-                  <div className="flex flex-row gap-1 py-2 align-bottom text-sm">
-                    <FiUser className="mt-1 " />
-                    <span>{}</span>
-                    {item.opd ? <span>{item.opd}</span> : <span> - </span>}
+                    <p className="!my-1 text-center text-base font-bold">
+                      {item.title}
+                    </p>
+                    <p
+                      className="!my-1 text-sm"
+                      dangerouslySetInnerHTML={{ __html: item.desc }}
+                    />
+                    <div className="flex flex-row gap-1 py-2 align-bottom text-sm">
+                      <FiUser className="mt-1 " />
+                      <span>{item.opd ? item.opd.name : ' - '}</span>
+                    </div>
+                    <div className="flex flex-row gap-1 py-2 align-bottom text-sm">
+                      <FiPackage className="mt-1 " />
+                      <span>{item.grup ? item.grup.name : ' - '}</span>
+                    </div>
+                    <div className="flex justify-between text-sm">
+                      <button
+                        className="flex w-24 items-center justify-center rounded-md bg-[#E8AEE2] px-2 py-1 font-bold text-black"
+                        onClick={() => handleLihatClick(item)}
+                      >
+                        <span>Lihat</span>
+                      </button>
+
+                      {item.url && ( // Conditionally render the button
+                        <button
+                          className="flex w-24  items-center justify-center rounded-md bg-[#AEE8B4] px-2 py-1 font-bold text-black"
+                          onClick={() => handleDownloadClick(item)}
+                        >
+                          <span>Download</span>
+                        </button>
+                      )}
+                    </div>
                   </div>
-                  <div className="flex flex-row gap-1 py-2 align-bottom text-sm">
-                    <FiPackage className="mt-1 " />
-                    <span>{}</span>
-                    {item.sektoral ? (
-                      <span>{item.sektoral}</span>
-                    ) : (
-                      <span> - </span>
-                    )}
-                  </div>
-                  <div className="flex justify-between text-sm">
-                    <button
-                      className="flex w-24 items-center justify-center rounded-md bg-[#E8AEE2] px-2 py-1 font-bold text-black"
-                      onClick={() => handleLihatClick(item)}
-                    >
-                      <span>Lihat</span>
-                    </button>
-                    <button
-                      className="flex w-24  items-center justify-center rounded-md bg-[#AEE8B4] px-2 py-1 font-bold text-black"
-                      onClick={() => handleDownloadClick(item)}
-                    >
-                      <span>Download</span>
-                    </button>
-                  </div>
-                </div>
-              ))
+                )
+              )
             )}
           </div>
         </div>
@@ -249,70 +272,89 @@ const Index = () => {
               <select
                 id="underline_select"
                 className="w-full rounded border-2 border-[#acacac] bg-transparent  text-gray-500 focus:border-[#acacac] focus:outline-none focus:ring-0 dark:border-gray-700 dark:text-gray-400"
-                value={show}
-                onChange={(event) => setShow(parseInt(event.target.value, 10))}
+                value={itemsPerPage}
+                onChange={handleItemsPerPageChange}
               >
                 <option value="8">8</option>
                 <option value="16">16</option>
                 <option value="32">32</option>
+                <option value="64">64</option>
+                <option value="120">120</option>
               </select>
             </div>
-            {data.length > 0 && (
-              <span>
-                Menampilkan data 1-{Math.min(show, data.length)} dari{' '}
-                {data.length}
-              </span>
-            )}
+            <span>Menampilkan data dari</span>
           </div>
-          <div>
-            {totalPages > 1 && (
-              <div className="mt-5">
-                {Array.from(Array(totalPages)).map((_, i) => (
+          <div className="mt-4 flex justify-center">
+            <ul className="flex gap-2">
+              {currentPage > 1 && (
+                <li>
                   <button
-                    key={i}
-                    onClick={() => handlePageChange(i + 1)}
-                    className={`mx-1 rounded-md border px-4 py-2 ${
-                      currentPage === i + 1
-                        ? 'bg-[#16A75C] text-white'
-                        : 'bg-white text-gray-700'
-                    }`}
+                    className="rounded bg-green-500 px-3 py-1 text-white"
+                    onClick={() => handlePageChange(currentPage - 1)}
                   >
-                    {i + 1}
+                    Prev
                   </button>
-                ))}
-              </div>
-            )}
+                </li>
+              )}
+              {Array.from({ length: totalPageCount }, (_, index) => {
+                const pageNumber = index + 1;
+                const isFirstPageInRange =
+                  pageNumber >= currentPage && pageNumber <= currentPage + 3;
+                if (isFirstPageInRange) {
+                  return (
+                    <li key={index}>
+                      <button
+                        className={`rounded px-3 py-1 ${
+                          currentPage === pageNumber
+                            ? 'bg-green-500 text-white'
+                            : 'bg-gray-200 text-gray-700'
+                        }`}
+                        onClick={() => handlePageChange(pageNumber)}
+                      >
+                        {pageNumber}
+                      </button>
+                    </li>
+                  );
+                }
+                return null;
+              })}
+              {currentPage + 3 < totalPageCount && (
+                <li>
+                  <button
+                    className="rounded bg-green-500 px-3 py-1 text-white"
+                    onClick={() => handlePageChange(currentPage + 4)}
+                  >
+                    Next
+                  </button>
+                </li>
+              )}
+            </ul>
           </div>
           {selectedItem && showModal && (
+            // eslint-disable-next-line tailwindcss/migration-from-tailwind-2
             <div className="fixed inset-0 z-50 bg-black bg-opacity-50">
               <div className="absolute left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-2 text-sm">
                 <video controls className="w-full rounded-md object-cover">
-                  <source src="dummyvideo.mp4" type="video/mp4" />
-                  Sorry, your browser doesn't support embedded videos.
+                  <source src={selectedItem.url} type="video/mp4" />
+                  Sorry, your browser doesnt support embedded videos.
                 </video>
                 <div className="py-1 text-center font-bold">
-                  {selectedItem.judul}
+                  {selectedItem.title}
                 </div>
-                <div className="my-1">{selectedItem.description}</div>
+                <div className="my-1">{selectedItem.desc}</div>
                 <div className="pt-2">
                   <div className="flex flex-row gap-1 align-bottom text-sm">
                     <FiUser className="mt-1 " />
-                    <span>{}</span>
-                    {selectedItem.opd ? (
-                      <span>{selectedItem.opd}</span>
-                    ) : (
-                      <span> - </span>
-                    )}
+                    <span>
+                      {selectedItem.opd ? selectedItem.opd.name : ' - '}
+                    </span>
                   </div>
                 </div>
                 <div className="flex flex-row gap-1 align-bottom text-sm">
                   <FiPackage className="mt-1 " />
-                  <span>{}</span>
-                  {selectedItem.sektoral ? (
-                    <span>{selectedItem.sektoral}</span>
-                  ) : (
-                    <span> - </span>
-                  )}
+                  <span>
+                    {selectedItem.grup ? selectedItem.grup.name : ' - '}
+                  </span>
                 </div>
                 <div className="flex justify-end">
                   <button
